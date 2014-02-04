3781af4ae84874d04e9be5a9cd9e770e35766d49
Andreas Bruun Okholm <aokholm@gmail.com>
2014-02-04 10:11:06 +0100

    Formated Measurement Helper

--- mobile-api/src/main/java/com/vaavud/server/analysis/post/MeasurementHelper.java
+++ mobile-api/src/main/java/com/vaavud/server/analysis/post/MeasurementHelper.java
@@ -20,7 +20,6 @@ import com.vaavud.sensor.Sensor;
 import com.vaavud.sensor.Sensor.Type;
 import com.vaavud.sensor.SensorEvent;
 import com.vaavud.sensor.SensorEvent1D;
-import com.vaavud.sensor.SensorEvent3D;
 import com.vaavud.server.model.Model;
 import com.vaavud.server.model.entity.Device;
 import com.vaavud.server.model.entity.MagneticSession;
@@ -28,95 +27,87 @@ import com.vaavud.server.model.entity.MeasurementPoint;
 import com.vaavud.server.model.entity.MeasurementSession;
 
 public class MeasurementHelper {
-    
+
     private MeasurementSession measurementSession;
     private Device device;
     private MagneticSession magneticSession;
     private double endTime;
-    private List<SensorEvent3D> magEvents = null;
     private List<SensorEvent> events = null;
-    
+    private EnumMap<Sensor.Type, Map<String, List<SensorEvent>>> eventMap;
+
     public MeasurementHelper(HttpServletRequest request) {
-        
+
         // Start hibernate Session
         Session hibernateSession = Model.get().getSessionFactory().openSession();
 
         if (request.getParameter("session_id") == null) {
-            measurementSession = (MeasurementSession) hibernateSession
-                    .createQuery(
-                            "from MeasurementSession order by id DESC LIMIT 1")
-                    .uniqueResult();
+            measurementSession = (MeasurementSession) hibernateSession.createQuery(
+                    "from MeasurementSession order by id DESC LIMIT 1").uniqueResult();
         } else {
-            measurementSession = (MeasurementSession) hibernateSession.get(
-                    MeasurementSession.class,
+            measurementSession = (MeasurementSession) hibernateSession.get(MeasurementSession.class,
                     Long.parseLong(request.getParameter("session_id")));
         }
-        
-        
+
         /* Get Device */
         device = measurementSession.getDevice();
-        
+
         /* Get magnetic Session */
         Query query = hibernateSession
                 .createQuery("from MagneticSession where measurementSessionUuid = :measurementSessionUuid");
-        query = query.setParameter("measurementSessionUuid",
-                measurementSession.getUuid());
+        query = query.setParameter("measurementSessionUuid", measurementSession.getUuid());
         magneticSession = (MagneticSession) query.uniqueResult();
 
-        
-        
-        List<SensorEvent1D> windEvents = generateWindEvents(measurementSession.getPoints()); 
+        List<SensorEvent1D> windEvents = generateWindEvents(measurementSession.getPoints());
         events = new ArrayList<>();
         events.addAll(windEvents);
 
-        
         if (magneticSession != null) {
             MeasurementAnalyzer analyzer = new MeasurementAnalyzer(magneticSession);
             events.addAll(analyzer.getEvents());
-            
+
         }
-            
-        //End timestep shown
-        endTime =  windEvents.get(windEvents.size()-1).getTime();
-        if (magEvents != null) {
-            if (magEvents.get(magEvents.size() - 1).getTime() > endTime) {
-                endTime = magEvents.get(magEvents.size() - 1).getTime();
-            }
-        }
-        
-        
-        
-        if (hibernateSession.getTransaction() != null
-                && hibernateSession.getTransaction().isActive()) {
+
+        // End timestep shown END TIME COULD BE LARGER (X,Y,Z)
+        endTime = windEvents.get(windEvents.size() - 1).getTime();
+        // if (magEvents != null) {
+        // if (magEvents.get(magEvents.size() - 1).getTime() > endTime) {
+        // endTime = magEvents.get(magEvents.size() - 1).getTime();
+        // }
+        // }
+
+        if (hibernateSession.getTransaction() != null && hibernateSession.getTransaction().isActive()) {
             hibernateSession.getTransaction().rollback();
         }
         hibernateSession.close();
-        
+
     }
-    
+
     public List<SensorEvent1D> generateWindEvents(List<MeasurementPoint> mesPoints) {
         // create wind time array // MP.time-MS.startTime)/1000
         Sensor sensor = new Sensor(Type.WINDSPEED, "Original");
         List<SensorEvent1D> windEvents = new ArrayList<>(mesPoints.size());
-        
+
         for (MeasurementPoint mesPoint : mesPoints) {
             long timeUs = (long) (mesPoint.getTime().getTime() - measurementSession.getStartTime().getTime()) * 1_000;
             windEvents.add(new SensorEvent1D(sensor, timeUs, mesPoint.getWindSpeed()));
         }
-        
+
         return windEvents;
     }
-    
+
     public String getDataTable() {
-     // public static CharSequence renderDataTable(DataTable dataTable, boolean includeValues, boolean includeFormatting, boolean renderDateAsDateConstructor)
-        
+        // public static CharSequence renderDataTable(DataTable dataTable,
+        // boolean includeValues, boolean includeFormatting, boolean
+        // renderDateAsDateConstructor)
         return JsonRenderer.renderDataTable(generateDataTable(), true, true, false).toString();
     }
-    
+
     public DataTable generateDataTable() {
         // Create a data table,
         DataTable data = new DataTable();
         ArrayList<ColumnDescription> cds = new ArrayList<ColumnDescription>();
+
+        // AUTOGENERATE THIS LIST
         cds.add(new ColumnDescriptionEvent("time", null, EventField.TIME, null));
         cds.add(new ColumnDescriptionEvent("windspeed", Type.WINDSPEED, EventField.VALUE, null));
         cds.add(new ColumnDescriptionEvent("magx", Type.MAGNETIC_FIELD, EventField.X, null));
@@ -129,76 +120,73 @@ public class MeasurementHelper {
         cds.add(new ColumnDescriptionEvent("f1_Freq", Type.FREQUENCY, EventField.FREQ, "Freq_1"));
         cds.add(new ColumnDescriptionEvent("f1_amp", Type.FREQUENCY, EventField.AMP, "Freq_1"));
         cds.add(new ColumnDescriptionEvent("f1_sf", Type.FREQUENCY, EventField.SF, "Freq_1"));
-        
+
         data.addColumns(cds);
 
-        
         List<SensorEvent> sortedEvents = sortList(events);
-        
+
         // Fill the data table.
         try {
             for (SensorEvent event : sortedEvents) {
                 Object[] row = new Object[cds.size()];
-                
+
                 int i = 0;
                 for (ColumnDescription cd : cds) {
                     ColumnDescriptionEvent colDes = (ColumnDescriptionEvent) cd;
                     if (colDes.getSensorType() == null) {
                         row[i] = colDes.getEventField().get(event);
-                    }
-                    else if (colDes.getSensorType() == event.getSensor().getType()) {
+                    } else if (colDes.getSensorType() == event.getSensor().getType()) {
                         if (colDes.getSensorName() == null || colDes.getSensorName() == event.getSensor().getName()) {
                             row[i] = colDes.getEventField().get(event);
                         }
                     }
                     i++;
                 }
-                
+
                 data.addRowFromValues(row);
             }
-          
+
         } catch (TypeMismatchException e) {
-          System.out.println("Invalid type!");
+            System.out.println("Invalid type!");
         }
         return data;
-      }
-    
-    
-    
+    }
+
     private List<SensorEvent> sortList(List<SensorEvent> events) {
-        
-        EnumMap<Sensor.Type, Map<String, List<SensorEvent>>> eventMap = new EnumMap<>(Sensor.Type.class);
-        for (Sensor.Type sensorType : Sensor.Type.values()) {
-            eventMap.put(sensorType, new HashMap<String, List<SensorEvent>>());
-        }
-        
-        for (SensorEvent event: events) {
+
+        eventMap = new EnumMap<>(Sensor.Type.class);
+
+        for (SensorEvent event : events) {
             Type eType = event.getSensor().getType();
             String eName = event.getSensor().getName();
-            if (! eventMap.get(eType).containsKey(eName)) {
+
+            if (!eventMap.containsKey(eType)) {
+                eventMap.put(eType, new HashMap<String, List<SensorEvent>>());
+            }
+
+            if (!eventMap.get(eType).containsKey(eName)) {
                 eventMap.get(eType).put(eName, new ArrayList<SensorEvent>());
             }
             eventMap.get(eType).get(eName).add(event);
         }
-        
+
         List<SensorEvent> sortedEvents = new ArrayList<SensorEvent>(events.size());
-        
-        for (Sensor.Type sensorType : Sensor.Type.values()) {
-            for (Map.Entry<String, List<SensorEvent>> entry : eventMap.get(sensorType).entrySet())
-            sortedEvents.addAll(entry.getValue());
+
+        for (Sensor.Type sensorType : eventMap.keySet()) {
+            for (List<SensorEvent> values : eventMap.get(sensorType).values())
+                sortedEvents.addAll(values);
         }
-        
+
         return sortedEvents;
     }
-    
+
     public String getTable(String title, Object object) {
-        
+
         StringBuilder sb = new StringBuilder(4000);
-        
-        
+
         sb.append("<h2>").append(title).append("</h2>");
         sb.append("<table>");
-        
+
         if (object != null) {
             for (Field field : object.getClass().getDeclaredFields()) {
                 field.setAccessible(true);
@@ -215,35 +203,30 @@ public class MeasurementHelper {
                 }
                 if (name == "device") {
                     addRow(sb, "Device", "...");
-                }
-                else if (name == "points") {
+                } else if (name == "points") {
                     addRow(sb, "points", "...");
-                }
-                else if (name == "magneticPoints") {
+                } else if (name == "magneticPoints") {
                     addRow(sb, "magneticPoints", "...");
-                }
-                else if (value == null) {
+                } else if (value == null) {
                     System.out.println(name);
-                }
-                else {
+                } else {
                     addRow(sb, name, value.toString());
                 }
             }
         }
         sb.append("</table>");
-        
+
         return sb.toString();
-        
+
     }
-    
+
     public void addRow(StringBuilder sb, String field, String value) {
-        sb.append("<tr>"); 
+        sb.append("<tr>");
         sb.append("<td>").append(field).append("</td>");
         sb.append("<td>").append(value).append("</td>");
         sb.append("</tr>");
     }
 
-    
     // GETTERS
     public MeasurementSession getMeasurementSession() {
         return measurementSession;
@@ -256,27 +239,25 @@ public class MeasurementHelper {
     public MagneticSession getMagneticSession() {
         return magneticSession;
     }
-    
+
     public Double getLatitude() {
         if (measurementSession.getPosition() == null) {
             return 0d;
-        }
-        else {
+        } else {
             return measurementSession.getPosition().getLatitude();
         }
-        
+
     }
-    
+
     public Double getLongitude() {
         if (measurementSession.getPosition() == null) {
             return 0d;
-        }
-        else {
+        } else {
             return measurementSession.getPosition().getLongitude();
         }
-        
+
     }
-    
+
     public Double getEndTime() {
         return endTime;
     }
