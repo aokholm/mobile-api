4dcc5662c6d7ab0cd9fcc5c367eab169388f545c
Thomas Stilling Ambus <thomas.ambus@vaavud.com>
2014-05-08 16:00:05 +0200

    History sync service.

--- mobile-api/src/main/java/com/vaavud/server/api/mobile/HistoryService.java
+++ mobile-api/src/main/java/com/vaavud/server/api/mobile/HistoryService.java
@@ -2,9 +2,15 @@ package com.vaavud.server.api.mobile;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,22 +30,111 @@ import com.vaavud.server.model.entity.Device;
 import com.vaavud.server.model.entity.LatLng;
 import com.vaavud.server.model.entity.MeasurementPoint;
 import com.vaavud.server.model.entity.MeasurementSession;
+import com.vaavud.util.UUIDUtil;
 
 public class HistoryService extends AbstractJSONService<HistoryService.RequestParameters> {
 
 	private static final Logger logger = Logger.getLogger(HistoryService.class);
 	
-	private static final long TIME_LIMIT_MILLIS = 24L * 3600L * 1000L;
-	
 	public static class RequestParameters implements Serializable {
-		private Date startTime;
+		
+		private Date latestStartTime;
+		private String hashedUUIDs;
+		
+		public Date getLatestStartTime() {
+			return latestStartTime;
+		}
+		
+		public void setLatestStartTime(Date latestStartTime) {
+			this.latestStartTime = latestStartTime;
+		}
+		
+		public String getHashedUUIDs() {
+			return hashedUUIDs;
+		}
+		
+		public void setHashedUUIDs(String hashedUUIDs) {
+			this.hashedUUIDs = hashedUUIDs;
+		}
+		
+		@Override
+		public String toString() {
+			return "RequestParameters [latestStartTime=" + latestStartTime
+					+ ", hashedUUIDs=" + hashedUUIDs + "]";
+		}
+	}
+	
+	public static class ResponseObject implements Serializable {
 
+		private String uuid;
+	    private Date startTime;
+	    private Double latitude;
+	    private Double longitude;
+	    private Float windSpeedAvg;
+	    private Float windSpeedMax;
+	    
+	    public ResponseObject() {
+	    }
+	    
+	    public ResponseObject(MeasurementSession measurementSession) {
+	    	this.uuid = measurementSession.getUuid();
+	    	this.startTime = measurementSession.getStartTime();
+	    	if (measurementSession.getPosition() != null &&
+	    			measurementSession.getPosition().getLatitude() != null && measurementSession.getPosition().getLongitude() != null &&
+	    			measurementSession.getPosition().getLatitude() != 0D && measurementSession.getPosition().getLongitude() != 0D) {
+	    		this.latitude = measurementSession.getPosition().getLatitude();
+	    		this.longitude = measurementSession.getPosition().getLongitude();
+	    	}
+	    	this.windSpeedAvg = measurementSession.getWindSpeedAvg();
+	    	this.windSpeedMax = measurementSession.getWindSpeedMax();
+	    }
+	    
+		public String getUuid() {
+			return uuid;
+		}
+		
+		public void setUuid(String uuid) {
+			this.uuid = uuid;
+		}
+		
 		public Date getStartTime() {
 			return startTime;
 		}
-
+		
 		public void setStartTime(Date startTime) {
 			this.startTime = startTime;
+		}
+		
+		public Double getLatitude() {
+			return latitude;
+		}
+		
+		public void setLatitude(Double latitude) {
+			this.latitude = latitude;
+		}
+		
+		public Double getLongitude() {
+			return longitude;
+		}
+		
+		public void setLongitude(Double longitude) {
+			this.longitude = longitude;
+		}
+		
+		public Float getWindSpeedAvg() {
+			return windSpeedAvg;
+		}
+		
+		public void setWindSpeedAvg(Float windSpeedAvg) {
+			this.windSpeedAvg = windSpeedAvg;
+		}
+		
+		public Float getWindSpeedMax() {
+			return windSpeedMax;
+		}
+		
+		public void setWindSpeedMax(Float windSpeedMax) {
+			this.windSpeedMax = windSpeedMax;
 		}
 	}
 			
@@ -50,23 +145,54 @@ public class HistoryService extends AbstractJSONService<HistoryService.RequestPa
 	
 	@Override
 	protected void process(HttpServletRequest req, HttpServletResponse resp, Device authenticatedDevice, RequestParameters object, ObjectMapper mapper, Session hibernateSession) throws UnauthorizedException, ProtocolException, IOException {
-		if (object == null) {
-			logger.info("Process object (null)");
+		logger.info("Process object " + object);
+
+		if (authenticatedDevice.getUser() == null) {
+			logger.error("User must be logged in");
+			throw new UnauthorizedException();
 		}
-		else {
-			logger.info("Process object " + object);
-
-			Date startTime = (object == null) ? null : object.getStartTime();
-
-			if (startTime == null || startTime.getTime() < (System.currentTimeMillis() - TIME_LIMIT_MILLIS)) {
-				logger.warn("Start time is before history time limit, using 24 hours back instead");
-				startTime = new Date(System.currentTimeMillis() - TIME_LIMIT_MILLIS);
-			}
-
-			List<Object[]> measurements = hibernateSession.createSQLQuery("select latitude, longitude, startTime, windSpeedAvg, windSpeedMax from MeasurementSession where startTime>:startTime and latitude is not null and longitude is not null and windSpeedAvg is not null").setLong("startTime", startTime.getTime()).list();
+		
+		logger.info("User " + authenticatedDevice.getUser().getEmail() + " and ID " + authenticatedDevice.getUser().getId());
+		
+		Date returnMeasurementsFrom = new Date(0L);
+		
+		if (object != null && object.getLatestStartTime() != null && object.getHashedUUIDs() != null) {
+			@SuppressWarnings("unchecked")
+			List<String> uuids = (List<String>) hibernateSession.createQuery(
+					"select s.uuid from MeasurementSession s where s.device.user.id=:userId and s.startTime<=:startTime order by s.startTime")
+					.setLong("userId", authenticatedDevice.getUser().getId())
+					.setLong("startTime", object.getLatestStartTime().getTime()).list();
 			
-			writeJSONResponse(resp, mapper, measurements);
+			StringBuilder sb = new StringBuilder(uuids.size() * 36);
+			for (String uuid : uuids) {
+				sb.append(uuid);
+			}
+			String serverHashedUUIDs = UUIDUtil.md5Hash(sb.toString().toUpperCase(Locale.US));
+			logger.info("Server hashed UUIDs: " + serverHashedUUIDs + " until " + (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(object.getLatestStartTime())) + " (" + object.getLatestStartTime().getTime() + "), client hashed UUIDs: " + object.getHashedUUIDs());
+			
+			if (serverHashedUUIDs.equals(object.getHashedUUIDs())) {
+				returnMeasurementsFrom = object.getLatestStartTime();
+			}
 		}
+
+		@SuppressWarnings("unchecked")
+		List<MeasurementSession> measurements = (List<MeasurementSession>) hibernateSession.createQuery(
+				"select s from MeasurementSession s where s.device.user.id=:userId and s.startTime>:startTime")
+				.setLong("userId", authenticatedDevice.getUser().getId())
+				.setLong("startTime", returnMeasurementsFrom.getTime()).list();
+		
+		logger.info("Found " + measurements.size() + " measurements");
+		
+		List<ResponseObject> responseList = new ArrayList<ResponseObject>(measurements.size());
+		for (MeasurementSession measurementSession : measurements) {
+			responseList.add(new ResponseObject(measurementSession));
+		}
+		
+		Map<String,Object> responseMap = new HashMap<String,Object>();
+		responseMap.put("fromStartTime", returnMeasurementsFrom);
+		responseMap.put("measurements", responseList);
+		
+		writeJSONResponse(resp, mapper, responseMap);
 	}
 
 	@Override
--- mobile-api/src/main/java/com/vaavud/util/UUIDUtil.java
+++ mobile-api/src/main/java/com/vaavud/util/UUIDUtil.java
@@ -1,21 +1,41 @@
 package com.vaavud.util;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 import java.util.UUID;
+
+import org.apache.log4j.Logger;
 
 public final class UUIDUtil {
 
-	//-------------------------------------------------------------------------
-	// Class methods
-	//-------------------------------------------------------------------------
+	private static final Logger logger = Logger.getLogger(UUIDUtil.class);
 
 	public static String generateUUID() {
 		return UUID.randomUUID().toString();
 	}
+	
+	public static String md5Hash(String text) {
+		String digest = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] hash = md.digest(text.getBytes("UTF-8")); 
+			StringBuilder sb = new StringBuilder(2*hash.length);
+			for(byte b : hash){
+				sb.append(String.format("%02x", b&0xff)); 
+			}
+			digest = sb.toString().toUpperCase(Locale.US);
+		}
+		catch (UnsupportedEncodingException e) {
+			logger.error(e);
+		}
+		catch (NoSuchAlgorithmException e) {
+			logger.error(e);
+		}
+		return digest;
+	}
 		
-	//-------------------------------------------------------------------------
-	// Constructors
-	//-------------------------------------------------------------------------
-
 	private UUIDUtil() {
 	}
 }
